20abd7207ebe7021b9293f91928a16d8f204776a
Louis-Elie Maillard <louiseliemaillard@gmail.com>
2018-04-10 16:22:37 +0200

    exo 3 pas du tout ok

--- exo3.rb
+++ exo3.rb
@@ -1,5 +1,13 @@
 trader_du_dimanche = [17,3,6,9,15,8,6,1,10]
-trader_du_dimanche.each {|selection|
-if selection =}
-selection[1][4]
- # $15 - $3 == $12
+
+#puts trader_du_dimanche.values_at(1, 3, 0)
+
+#puts trader_du_dimanche.min
+
+#puts trader_du_dimanche.max
+
+
+
+puts trader_du_dimanche.max - trader_du_dimanche.min
+
+
